1e542b2c5f90939faffc5511339ff83840d66953
LeoLeBG <leo.bauduin@epitech.eu>
2023-04-02 16:50:46 +0200

    TEST coding style and push

--- src/main.c
+++ src/main.c
@@ -7,8 +7,7 @@
 
 #include "minishell2.h"
 
-static int check_error(int ac, char **av)
-{
+static int check_error(int ac, char *      *            av) {
     if (ac == 2 && my_strcmp(av[1], "-h") == 0) {
         my_printf("USAGE : ./mysh\n");
         return 0;
@@ -20,11 +19,14 @@ static int check_error(int ac, char **av)
     return 0;
 }
 
-int	main(int ac, char *av[], char *env[])
+int	main(int ac, char *          av[], char *              env[])
 {
     global_t global;
 
     if (check_error(ac, av))
+
+
+
         return 84;
     init_global(&global, env);
 
@@ -32,6 +34,8 @@ int	main(int ac, char *av[], char *env[])
         get_str_prompt(&global);
         my_printf("~%s $> ", global.prompt);
     }
+
+                    
     mysh(&global);
     if (isatty(STDIN_FILENO))
         my_printf("Good Bye %s !\n", global.name);
